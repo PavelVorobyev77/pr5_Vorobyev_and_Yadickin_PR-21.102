34177911f6c09bfe919755871f68fe357b7ccb07
PavelVorobyev77 <92383863+PavelVorobyev77@users.noreply.github.com>
2023-03-10 13:55:10 +0700

    Add files via upload

---
+++ pr5VorobyevandYadikin/app/src/main/java/com/example/pr5vorobyevandyadikin/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.pr5vorobyevandyadikin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }//for commit
+//gwerdvcsdvsdvsdvsdvvwv
+    //hfchjcghjchm
+    //changed main1
+    //changed main2
+    //changed main3
+    //changed main4
+
+
+}
